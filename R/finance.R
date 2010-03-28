3b685090484eb89482125d5e54b6b287eb1d0460
Brian Rowe <rowe@muxspace.com>
2010-03-28 21:44:54 +0000

    Removed unneeded source files

--- R/finance.R
+++
@@ -1,9 +0,0 @@
-# Get a HLC matrix from a quantmod OHLC (or for any matter any other matrix
-# that has superfluous columns)
-hlc <- function(market)
-{
-  require(xts, quietly=TRUE)
-  cbind(Hi(market), Lo(market), Cl(market))
-}
-
-
