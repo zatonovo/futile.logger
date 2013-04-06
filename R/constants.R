d3da138b8d54168481505113147f9539eceb6e55
Alexandre Sieira <alexandre.sieira@gmail.com>
2013-04-06 10:52:09 -0300

    Assigning names to constants
    By assigning names to the constants, custom layout functions will be
    able to easily determine the string equivalent. After all, .LOG_LEVEL
    is not accessible outside the package namespace.

--- R/constants.R
+++ R/constants.R
@@ -1,9 +1,15 @@
 FATAL <- 1
+names(FATAL) <- "FATAL"
 ERROR <- 2
+names(ERROR) <- "ERROR"
 WARN <- 4
+names(WARN) <- "WARN"
 INFO <- 6
+names(INFO) <- "INFO"
 DEBUG <- 8
+names(DEBUG) <- "DEBUG"
 TRACE <- 9
+names(TRACE) <- "TRACE"
 
 .LOG_LEVEL <- c('FATAL','ERROR',NA,'WARN', NA,'INFO', NA,'DEBUG','TRACE')
 
