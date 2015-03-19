3eff14bc6116bf2f5ea6236eba9db8b79085672f
Harlan Harris <hharris@eab.com>
2015-03-19 12:59:12 -0400

    import jsonlite on package load; tests pass

--- R/futile.logger-package.R
+++ R/futile.logger-package.R
@@ -90,7 +90,7 @@
 #' @aliases futile.logger-package futile.logger flog.namespace
 #' @docType package
 #' @exportPattern "^[^\\.]"
-#' @import lambda.r futile.options
+#' @import lambda.r futile.options jsonlite
 #' @author Brian Lee Yung Rowe <r@@zatonovo.com>
 #' @seealso \code{\link{flog.logger}}, \code{\link{flog.threshold}},
 #' \code{\link{flog.layout}}, \code{\link{flog.appender}}
