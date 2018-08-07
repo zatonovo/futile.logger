f3690484062b99d13d74913fd2fe58589bcc4ef1
wesnest <osumowesley@gmail.com>
2018-08-07 15:09:28 -0500

    made some changes to the appender

--- R/appender.R
+++ R/appender.R
@@ -98,112 +98,14 @@ flog.appender(fn, name='ROOT') %as%
   invisible()
 }
 
-# Some default handlers for use in futile.logger. All handlers need to conform
-# to the below signature: function(line)
-appender.console <- function()
-{
-  function(line) cat(line, sep='')
-}
 
-# Write to a file.
-appender.file <- function(file)
-{
-  function(line) cat(line, file=file, append=TRUE, sep='')
-}
-
-# Write to a file and to console 
-appender.tee <- function(file){ 
-  function(line) {
-    cat(line, sep='') 
-    cat(line, file=file, append=TRUE, sep='')
-  }
-}
-
-# Write to a dynamically-named file (and optionally the console), with inheritance
-appender.file2 <- function(format, console=FALSE, inherit=TRUE,
-                           datetime.fmt="%Y%m%dT%H%M%S") {
-  .nswhere <- -3 # get name of the function 2 deep in the call stack
-                 # that is, the function that has called flog.*
-  .funcwhere <- -3 # ditto for the function name
-  .levelwhere <- -1 # ditto for the current "level"
-  function(line) {
-    if (console) cat(line, sep='')
-    err <- function(e) {
-      stop('Illegal function call, must call from flog.trace, flog.debug, flog.info, flog.warn, flog.error, flog.fatal, etc.')
-    }
-    the.level <- tryCatch(get("level", envir=sys.frame(.levelwhere)),error = err)
-    the.threshold <- tryCatch(get('logger',envir=sys.frame(.levelwhere)), error=err)$threshold
-    if(inherit) {
-      LEVELS <- c(FATAL, ERROR, WARN, INFO, DEBUG, TRACE)
-      levels <- names(LEVELS[the.level <= LEVELS & LEVELS <= the.threshold])
-    } else levels <- names(the.level)
-    the.time <- format(Sys.time(), datetime.fmt)
-    the.namespace <- flog.namespace(.nswhere)
-    the.namespace <- ifelse(the.namespace == 'futile.logger', 'ROOT', the.namespace)
-    the.function <- .get.parent.func.name(.funcwhere)
-    the.pid <- Sys.getpid()
-    filename <- gsub('~t', the.time, format, fixed=TRUE)
-    filename <- gsub('~n', the.namespace, filename, fixed=TRUE)
-    filename <- gsub('~f', the.function, filename, fixed=TRUE)
-    filename <- gsub('~p', the.pid, filename, fixed=TRUE)
-    if(length(grep('~l', filename)) > 0) {
-      sapply(levels, function(level) {
-        filename <- gsub('~l', level, filename, fixed=TRUE)
-        cat(line, file=filename, append=TRUE, sep='')
-      })
-    }else cat(line, file=filename, append=TRUE, sep='')
-    invisible()
-  }
-}
-
-
-# Special meta appender that prints only when the internal counter mod n = 0
-appender.modulo <- function(n, appender=appender.console()) {
-  i <- 0
-  function(line) {
-    i <<- i + 1
-    if (i %% n == 0) appender(sprintf("[%s] %s", i,line))
-    invisible()
-  }
-}
-
-# Write to a Graylog2 HTTP GELF Endpoint
-appender.graylog <- function(server, port, debug = FALSE) {
-
-  if (!requireNamespace("jsonlite", quietly=TRUE))
-    stop("appender.graylog requires jsonlite. Please install it.", call. = FALSE)
-  if (!requireNamespace("httr", quietly=TRUE))
-    stop("appender.graylog requires httr. Please install it.", call. = FALSE)  
-  
-  function(line) {
-
-    ret <- httr::POST(paste0("http://", server, ":", port, "/gelf"), 
-                      body = list(short_message = line),
-                      encode = 'json')
-    
-    if (debug) print(ret)
-  }
-}
-appender.kinesis <- function(stream, region_name)
-{
-  function(line) {
-    #rcticloud::Firehose@put_record(stream, region = region_name, line)
-  }
-}
-
-appender.kinesis <- function(stream, region_name)
-{
-  function(line) {
-    #rcticloud::Firehose@put_record(stream, region = region_name, line)
-  }
-}
 
 #' Kinesis Firehose and console appender
 #' 
 #' @param stream Firehose stream name
 #' @param region_name Firehose stream region
 #' @export
-appender.kinesis_tee <- function(mykey, secret_key){ 
+appender.kinesis_firehose<- function(mykey, secret_key){ 
   library(rcticloud)
   function(line) {
     cat(line, sep='') 
